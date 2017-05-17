ec2dabb6fcea22d2eb2b319baaddaa3014582c17
Shantanu Karve <shantanu.karve@gmail.com>
2017-05-17 12:01:04 -0700

    deleted activity_main2.xml

--- app/src/main/java/com/adsmobeg/tarat/adsmobeg/MainActivity.java
+++ app/src/main/java/com/adsmobeg/tarat/adsmobeg/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         mAdView = (AdView) findViewById(R.id.adView2);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
